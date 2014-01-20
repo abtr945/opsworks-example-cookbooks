5ebd6da09262ef0d858b85c8c03a6087e1f7cfa7
doxu2620 <xjr1991@hotmail.com>
2014-01-20 14:53:10 +1100

    Create shutdown.rb

---
+++ tomcat/recipes/shutdown.rb
@@ -0,0 +1,22 @@
+#
+# Cookbookname: tomcat
+# Recipe:: shutdown.rb
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# Uninstall Tomcat
+script "uninstall_tomcat" do
+  interpreter "bash"
+  user "root"
+  code <<-EOH
+    tomcatv=`ps -eLf|grep tomcat | cut -d ' ' -f 1 | head -1`
+    if [[ $tomcatv == tomcat* ]];
+    then
+      sudo dpkg -P $tomcatv
+      sudo rm -r /usr/share/$tomcatv
+    if
+  EOH
+end
