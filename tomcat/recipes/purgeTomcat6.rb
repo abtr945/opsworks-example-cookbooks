facfbd90a2a0f65184f402ab064b28473a13dc24
doxu2620 <xjr1991@hotmail.com>
2014-01-28 16:43:54 +1100

    Update purgeTomcat6.rb

--- tomcat/recipes/purgeTomcat6.rb
+++ tomcat/recipes/purgeTomcat6.rb
@@ -1,4 +1,3 @@
-
 tomcat_pkgs = value_for_platform(
   ['debian', 'ubuntu'] => {
     'default' => ["tomcat#{node['tomcat']['base_version']}", 'libtcnative-1', 'libmysql-java']
@@ -9,8 +8,22 @@ tomcat_pkgs = value_for_platform(
   'default' => ["tomcat#{node['tomcat']['base_version']}"]
 )
 
+tomcat_version = "tomcat#{node['tomcat']['base_version']}"
+
 tomcat_pkgs.each do |pkg|
   package pkg do
     action :remove
   end
+end
+
+script "uninstall_tomcat" do
+  interpreter "bash"
+  user "root"
+  code <<-EOH
+    sudo rm -rf /usr/share/#{tomcat_version}
+    if [ -d "/var/lib/#{tomcat_version}" ]
+    then
+      sudo rm -rf /var/lib/#{tomcat_version}
+    fi
+  EOH
 end
