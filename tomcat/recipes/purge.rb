9e4cceec60b813c038ca596c59f8704131756832
doxu2620 <xjr1991@hotmail.com>
2014-02-18 15:42:40 +1100

    Update purge.rb

--- tomcat/recipes/purge.rb
+++ tomcat/recipes/purge.rb
@@ -1,17 +1,54 @@
+ruby_block "find-tomcat-old-version" do
+  block do
+    node['current_server'] = `ps -eLf|grep tomcat | cut -d ' ' -f 1 | head -1`
+    if node['current_server'].start_with? 'tomcat'
+      regex = /^tomcat(?<old_version>\d+)$/
+      parts = node['current_server'].match(regex)
+      node['tomcat']['old_version'] = parts['old_version']
+      puts "===================================#{node['tomcat']['old_version']}=============="
+    end
+  end
+  action :create
+end
+
 tomcat_pkgs = value_for_platform(
   ['debian', 'ubuntu'] => {
-    'default' => ["tomcat#{node['tomcat']['base_version']}", 'libtcnative-1', 'libmysql-java']
+    'default' => ["tomcat#{node['tomcat']['old_version']}", 'libtcnative-1', 'libmysql-java']
   },
   ['centos', 'redhat', 'fedora', 'amazon'] => {
-    'default' => ["tomcat#{node['tomcat']['base_version']}", 'tomcat-native', 'mysql-connector-java']
+    'default' => ["tomcat#{node['tomcat']['old_version']}", 'tomcat-native', 'mysql-connector-java']
   },
-  'default' => ["tomcat#{node['tomcat']['base_version']}"]
+  'default' => ["tomcat#{node['tomcat']['old_version']}"]
 )
 
+tomcat_old_version = "tomcat#{node['tomcat']['old_version']}"
+tomcat_current_version = "tomcat#{node['tomcat']['base_version']}"
 
 tomcat_pkgs.each do |pkg|
   package pkg do
-    action :remove
+    action :purge
   end
 end
 
+script "uninstall_tomcat" do
+  interpreter "bash"
+  user "root"
+  code <<-EOH
+    if [ -d "/usr/share/#{tomcat_old_version}" ]
+    then
+      sudo rm -rf /usr/share/#{tomcat_old_version}
+    fi
+    if [ -d "/usr/share/#{tomcat_current_version}" ]
+    then
+      sudo rm -rf /usr/share/#{tomcat_current_version}
+    fi
+    if [ -d "/var/lib/#{tomcat_old_version}" ]
+    then
+      sudo rm -rf /var/lib/#{tomcat_old_version}
+    fi
+    if [ -d "/var/lib/#{tomcat_current_version}" ]
+    then
+      sudo rm -rf /var/lib/#{tomcat_current_version}
+    fi
+  EOH
+end
