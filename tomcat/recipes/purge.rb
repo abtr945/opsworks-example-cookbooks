eb6b14be9a05f563e310ffd4844d4c27c2eba4b9
doxu2620 <xjr1991@hotmail.com>
2014-02-20 00:35:29 +1100

    Update purge.rb

--- tomcat/recipes/purge.rb
+++ tomcat/recipes/purge.rb
@@ -3,69 +3,58 @@ ruby_block "find-tomcat-old-version" do
     current_server = `ps -eLf|grep -Eo '^[^ ]+'| grep tomcat`
     if parts = current_server.match(/tomcat([0-9])/)
       ver = parts.captures.join("")
-      node.default['tomcat_old_server'] = ver
-      puts "************************#{node.default['tomcat_old_server']}****************"
-      puts "^^^^^^^^^^^^^^^^^In else statement, #{node['tomcat']['old_version']}^^^^^^^^^^^^^^"
+      node.default['tomcat']['old_server'] = ver
+      puts "************************#{node.default['tomcat']['old_server']}****************"
     else
-      node.default['tomcat_old_server'] = node['tomcat']['old_version']
+      node.default['tomcat']['old_server'] = node['tomcat']['base_version']
+      puts "^^^^^^^^^^^^^^^^^In else statement, #{node['tomcat']['old_version']}^^^^^^^^^^^^^^"
     end
   end
   action :create
 end
 
-ruby_block "print-tomcat-old-server" do
-  block do
-    puts "************************#{node['tomcat_old_server']}****************"
-    tomcat_pkgs = value_for_platform(
-    ['debian', 'ubuntu'] => {
-     'default' => ["tomcat#{node['tomcat_old_server']}", 'libtcnative-1', 'libmysql-java']
-    },
-    ['centos', 'redhat', 'fedora', 'amazon'] => {
-     'default' => ["tomcat#{node['tomcat_old_server']}", 'tomcat-native', 'mysql-connector-java']
-    },
-    'default' => ["tomcat#{node['tomcat_old_server']}"]
-    )
-    tomcat_old_version = "tomcat#{node['tomcat_old_server']}"
-    tomcat_current_version = "tomcat#{node['tomcat']['base_version']}"
-    tomcat_pkgs.each do |pkg|
-      package pkg do
-        action :purge
-      end
-    end
-    script "uninstall_tomcat" do
-      interpreter "bash"
-      user "root"
-      code <<-EOH
-        if [ -d "/usr/share/#{tomcat_old_version}" ]
-        then
-          sudo rm -rf /usr/share/#{tomcat_old_version}
-        fi
-        if [ -d "/usr/share/#{tomcat_current_version}" ]
-        then
-          sudo rm -rf /usr/share/#{tomcat_current_version}
-        fi
-        if [ -d "/var/lib/#{tomcat_old_version}" ]
-        then
-          sudo rm -rf /var/lib/#{tomcat_old_version}
-        fi
-        if [ -d "/var/lib/#{tomcat_current_version}" ]
-        then
-          sudo rm -rf /var/lib/#{tomcat_current_version}
-        fi
-      EOH
-    end
-    
+log "===============#{node['tomcat']['old_version']}=============================" do
+  level :info
+end
+
+tomcat_pkgs = value_for_platform(
+  ['debian', 'ubuntu'] => {
+    'default' => ["tomcat#{node['tomcat']['old_version']}", 'libtcnative-1', 'libmysql-java']
+  },
+  ['centos', 'redhat', 'fedora', 'amazon'] => {
+    'default' => ["tomcat#{node['tomcat']['old_version']}", 'tomcat-native', 'mysql-connector-java']
+  },
+  'default' => ["tomcat#{node['tomcat']['old_version']}"]
+)
+
+tomcat_old_version = "tomcat#{node['tomcat']['old_version']}"
+tomcat_current_version = "tomcat#{node['tomcat']['base_version']}"
+
+tomcat_pkgs.each do |pkg|
+  package pkg do
+    action :purge
   end
 end
 
-#log "===============#{node['tomcat_old_server']}=============================" do
-#  level :info
-#end
-
-
-
-
-
-
-
-
+script "uninstall_tomcat" do
+  interpreter "bash"
+  user "root"
+  code <<-EOH
+    if [ -d "/usr/share/#{tomcat_old_version}" ]
+    then
+      sudo rm -rf /usr/share/#{tomcat_old_version}
+    fi
+    if [ -d "/usr/share/#{tomcat_current_version}" ]
+    then
+      sudo rm -rf /usr/share/#{tomcat_current_version}
+    fi
+    if [ -d "/var/lib/#{tomcat_old_version}" ]
+    then
+      sudo rm -rf /var/lib/#{tomcat_old_version}
+    fi
+    if [ -d "/var/lib/#{tomcat_current_version}" ]
+    then
+      sudo rm -rf /var/lib/#{tomcat_current_version}
+    fi
+  EOH
+end
