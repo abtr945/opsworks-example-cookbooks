d842506ac9ceb748455e82c77f1e4812c3bc6917
doxu2620 <xjr1991@hotmail.com>
2014-02-20 22:00:32 +1100

    Update shutdown.rb

--- tomcat/recipes/shutdown.rb
+++ tomcat/recipes/shutdown.rb
@@ -1,11 +1,11 @@
 tomcat_pkgs = value_for_platform(
   ['debian', 'ubuntu'] => {
-    'default' => ["tomcat#{node['tomcat']['base_version']}", 'libtcnative-1', 'libmysql-java']
+    'default' => ["tomcat#{node['tomcat']['old_version']}", 'libtcnative-1', 'libmysql-java']
   },
   ['centos', 'redhat', 'fedora', 'amazon'] => {
-    'default' => ["tomcat#{node['tomcat']['base_version']}", 'tomcat-native', 'mysql-connector-java']
+    'default' => ["tomcat#{node['tomcat']['old_version']}", 'tomcat-native', 'mysql-connector-java']
   },
-  'default' => ["tomcat#{node['tomcat']['base_version']}"]
+  'default' => ["tomcat#{node['tomcat']['old_version']}"]
 )
 
 tomcat_old_version = "tomcat#{node['tomcat']['old_version']}"
@@ -13,7 +13,7 @@ tomcat_current_version = "tomcat#{node['tomcat']['base_version']}"
 
 tomcat_pkgs.each do |pkg|
   package pkg do
-    action :remove
+    action :purge
   end
 end
 
