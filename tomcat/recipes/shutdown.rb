ae786712214b63040d0b2028f140294b10e62f8c
doxu2620 <xjr1991@hotmail.com>
2014-02-21 14:13:47 +1100

    Update shutdown.rb

--- tomcat/recipes/shutdown.rb
+++ tomcat/recipes/shutdown.rb
@@ -7,15 +7,6 @@ tomcat_pkgs = value_for_platform(
   },
   'default' => ["tomcat#{node['tomcat']['old_version']}"]
 )
-tomcat_current_pkgs = value_for_platform(
-  ['debian', 'ubuntu'] => {
-    'default' => ["tomcat#{node['tomcat']['base_version']}", 'libtcnative-1', 'libmysql-java']
-  },
-  ['centos', 'redhat', 'fedora', 'amazon'] => {
-    'default' => ["tomcat#{node['tomcat']['base_version']}", 'tomcat-native', 'mysql-connector-java']
-  },
-  'default' => ["tomcat#{node['tomcat']['base_version']}"]
-)
 
 tomcat_old_version = "tomcat#{node['tomcat']['old_version']}"
 tomcat_current_version = "tomcat#{node['tomcat']['base_version']}"
@@ -26,10 +17,8 @@ tomcat_pkgs.each do |pkg|
   end
 end
 
-tomcat_current_pkgs.each do |pkg|
-  package pkg do
-    action :purge
-  end
+package "tomcat#{node['tomcat']['base_version']}" do
+  action :purge
 end
 
 script "uninstall_tomcat" do
