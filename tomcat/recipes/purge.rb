62f3b73621ff014ad483f1b65a7ea25f0cc7ed6c
doxu2620 <xjr1991@hotmail.com>
2014-02-25 20:02:55 +1100

    Update purge.rb

--- tomcat/recipes/purge.rb
+++ tomcat/recipes/purge.rb
@@ -1,62 +1,16 @@
-ruby_block "find-tomcat-old-version" do
-  block do
-    current_server = `ps -eLf|grep -Eo '^[^ ]+'| grep tomcat`
-    if parts = current_server.match(/tomcat([0-9])/)
-      ver = parts.captures.join("")
-      puts "previous---************************#{node['tomcat']['old_version']}****************"
-      set['tomcat']['old_version'] = ver
-      normal['tomcat']['old_version'] = ver
-      puts "after---************************#{node['tomcat']['old_version']}****************"
-    else
-      node.default['tomcat']['old_version'] = node['tomcat']['base_version']
-      puts "^^^^^^^^^^^^^^^^^In else statement, #{node['tomcat']['old_version']}^^^^^^^^^^^^^^"
-    end
-  end
-  action :create
-end
-
-log "===============#{normal['tomcat']['old_version']}=============================" do
-  level :info
-end
-
 tomcat_pkgs = value_for_platform(
   ['debian', 'ubuntu'] => {
-    'default' => ["tomcat#{node['tomcat']['old_version']}", 'libtcnative-1', 'libmysql-java']
+    'default' => ["tomcat#{node['tomcat']['base_version']}", 'libtcnative-1', 'libmysql-java']
   },
   ['centos', 'redhat', 'fedora', 'amazon'] => {
-    'default' => ["tomcat#{node['tomcat']['old_version']}", 'tomcat-native', 'mysql-connector-java']
+    'default' => ["tomcat#{node['tomcat']['base_version']}", 'tomcat-native', 'mysql-connector-java']
   },
-  'default' => ["tomcat#{node['tomcat']['old_version']}"]
+  'default' => ["tomcat#{node['tomcat']['base_version']}"]
 )
 
-tomcat_old_version = "tomcat#{node['tomcat']['old_version']}"
-tomcat_current_version = "tomcat#{node['tomcat']['base_version']}"
 
 tomcat_pkgs.each do |pkg|
   package pkg do
-    action :purge
+    action :remove
   end
-end
-
-script "uninstall_tomcat" do
-  interpreter "bash"
-  user "root"
-  code <<-EOH
-    if [ -d "/usr/share/#{tomcat_old_version}" ]
-    then
-      sudo rm -rf /usr/share/#{tomcat_old_version}
-    fi
-    if [ -d "/usr/share/#{tomcat_current_version}" ]
-    then
-      sudo rm -rf /usr/share/#{tomcat_current_version}
-    fi
-    if [ -d "/var/lib/#{tomcat_old_version}" ]
-    then
-      sudo rm -rf /var/lib/#{tomcat_old_version}
-    fi
-    if [ -d "/var/lib/#{tomcat_current_version}" ]
-    then
-      sudo rm -rf /var/lib/#{tomcat_current_version}
-    fi
-  EOH
 end
