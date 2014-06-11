76593fe7985d4a4e8a269bb77809cf16662cf98c
An Binh Tran <an.tran@student.unsw.edu.au>
2014-06-11 10:02:47 +1000

    Update shutdown.rb

--- tomcat/recipes/shutdown.rb
+++ tomcat/recipes/shutdown.rb
@@ -9,16 +9,11 @@ tomcat_pkgs = value_for_platform(
 )
 
 tomcat_old_version = "tomcat#{node['tomcat']['old_version']}"
-tomcat_current_version = "tomcat#{node['tomcat']['base_version']}"
 
 tomcat_pkgs.each do |pkg|
   package pkg do
     action :purge
   end
-end
-
-package "tomcat#{node['tomcat']['base_version']}" do
-  action :purge
 end
 
 script "uninstall_tomcat" do
@@ -29,17 +24,9 @@ script "uninstall_tomcat" do
     then
       sudo rm -rf /usr/share/#{tomcat_old_version}
     fi
-    if [ -d "/usr/share/#{tomcat_current_version}" ]
-    then
-      sudo rm -rf /usr/share/#{tomcat_current_version}
-    fi
     if [ -d "/var/lib/#{tomcat_old_version}" ]
     then
       sudo rm -rf /var/lib/#{tomcat_old_version}
-    fi
-    if [ -d "/var/lib/#{tomcat_current_version}" ]
-    then
-      sudo rm -rf /var/lib/#{tomcat_current_version}
     fi
   EOH
 end
