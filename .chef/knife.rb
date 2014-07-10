e0e39ad651b1a6f4d073e0f07f7c6f11a0b48ae5
An Binh Tran <anbinhtran@wlan-0a118de6.ipt.nicta.com.au>
2014-07-10 11:24:53 +1000

    delete

--- .chef/knife.rb
+++
@@ -1,9 +0,0 @@
-log_level                :info
-log_location             STDOUT
-node_name                "trananbinh110"
-client_key               "#{current_dir}/trananbinh110.pem"
-validation_client_name   "unsw-thesis---nicta-validator"
-validation_key           "#{current_dir}/unsw-thesis---nicta-validator.pem"
-chef_server_url          "https://api.opscode.com/organizations/unsw-thesis---nicta"
-syntax_check_cache_path  "#{current_dir}/syntax_check_cache"
-cookbook_path            [ "#{current_dir}/../cookbooks" ]
