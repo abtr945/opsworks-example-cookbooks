1430a7e8c4910f830ffa22146a958342f3bb4fba
An Binh Tran <an.tran@student.unsw.edu.au>
2014-07-10 10:18:37 +1000

    Update default.rb

--- hadoop/attributes/default.rb
+++ hadoop/attributes/default.rb
@@ -1,5 +1,5 @@
 default[:Hadoop][:Core][:hadoopNamenodeDir] = "/home/hduser/hdfs/namenode"
 default[:Hadoop][:Core][:hadoopDatanodeDir] = "/home/hduser/hdfs/datanode"
 override[:java][:install_flavor] = "oracle"
-override[:java][:accept_oracle_download_terms] = true
+override[:java][:oracle][:accept_oracle_download_terms] = true
 override[:java][:jdk_version] = "7"
