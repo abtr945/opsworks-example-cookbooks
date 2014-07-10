c7a7eac6da8495c39f8e48ce0d661922d56546a9
An Binh Tran <anbinhtran@wlan-0a118de6.ipt.nicta.com.au>
2014-07-10 11:12:44 +1000

    delete aws key

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -7,7 +7,3 @@ validation_key           "#{current_dir}/unsw-thesis---nicta-validator.pem"
 chef_server_url          "https://api.opscode.com/organizations/unsw-thesis---nicta"
 syntax_check_cache_path  "#{current_dir}/syntax_check_cache"
 cookbook_path            [ "#{current_dir}/../cookbooks" ]
-
-knife[:aws_access_key_id] = 'AKIAJ4K5FO3KGMGGPXOA'
-knife[:aws_ssh_key_id] = 'chef'
-knife[:aws_secret_access_key] = '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'
